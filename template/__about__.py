77f62f4820d2add7cd99356a290d0243f2897a69
tomato2718 <55865760+tomato2718@users.noreply.github.com>
2023-07-09 17:03:07 +0800

    Initial commit

---
+++ template/__about__.py
@@ -0,0 +1,26 @@
+'''
+Informations about the project.
+'''
+
+__all__ = [
+    '__project__',
+    '__version__',
+    '__author__',
+    '__maintainer__',
+    '__release__',
+    '__summary__',
+    '__usage__'
+]
+
+__project__ = 'template'
+__version__ = '0.0.0'
+__author__ = 'yveschen2718@gmail.com'
+__maintainer__ = 'yveschen2718@gmail.com'
+__release__ = '2023/07/09'
+__summary__ = 'Python project template'
+__usage__ = f'''
+Usage:
+    >>> 
+    >>> 
+    >>> 
+'''
