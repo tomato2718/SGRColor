d36e8cad131932b3a28b93917e7eb79efc28ca64
tomato2718 <qweasdzxc030@gmail.com>
2023-08-22 18:16:37 +0800

    Tests: Remove unnecessary test

--- tests/test__sgrcolor.py
+++ tests/test__sgrcolor.py
@@ -8,71 +8,29 @@ from sgrcolor._sgrcolor import SGRColor
 ##### TESTS #####
 class TestSGRColor:
     @staticmethod
-    @pytest.mark.parametrize(
-        "testcase",
-        [
-            {
-                'font': 'apple',
-                'fg': 'black',
-                'bg': 'black',
-            },
-            {
-                'font': 'bold',
-                'fg': 'banana',
-                'bg': 'black',
-            },
-            {
-                'font': 'bold',
-                'fg': 'black',
-                'bg': 'cherry',
-            },
-            {
-                'font': 1,
-                'fg': 'black',
-                'bg': 'black',
-            },
-            {
-                'font': 'bold',
-                'fg': [1],
-                'bg': 'black',
-            },
-            {
-                'font': 'bold',
-                'fg': 'black',
-                'bg': {2: 3},
-            },
-        ]
-    )
-    def test___init__(testcase):
-        with pytest.raises((TypeError, ValueError)):
-            SGRColor(**testcase)
-    
-    @staticmethod
     def test___call__():
         text = 'foo'
         # bold cyan None
-        testcase1 = '\033[1;36m' + text + '\033[0m'
+        testcase1 = f'\033[1;36m{text}\033[0m'
         # italic black white
-        testcase2 = '\033[3;30;47m' + text + '\033[0m'
+        testcase2 = f'\033[3;30;47m{text}\033[0m'
         # bold strike green None
-        testcase3 = '\033[1;9;32m' + text + '\033[0m'
+        testcase3 = f'\033[1;9;32m{text}\033[0m'
 
         test1 = SGRColor(
-            font='bold',
-            fg='cyan'
+            font='BOLD',
+            fg='CYAN',
         )
         test2 = SGRColor(
-            font='italic',
-            fg='black',
-            bg='white'
+            font='ITALIC',
+            fg='BLACK',
+            bg='WHITE',
         )
         test3 = SGRColor(
-            font=('bold', 'strike'),
-            fg='green'
+            font=('BOLD', 'STRIKE'),
+            fg='GREEN'
         )
 
         assert test1(text) == testcase1
         assert test2(text) == testcase2
         assert test3(text) == testcase3
-        with pytest.raises(TypeError):
-            test1(123)
